ea6d8b31f1307ace8a4395d5a8839091a41573ee
root <jpvillemin@gmail.com>
2022-12-08 14:27:33 +0100

    Add Api.R getInteraction

--- SCSR-application.R
+++ SCSR-application.R
@@ -5,19 +5,27 @@ baseDir  <- '/data/villemin'
 package <- '/data2/villemin/SingleCellSignalR/SingleCellSignalR'
 
 devtools::install(glue("{baseDir}{package}"))
-
+#library(SingleCellSignalR)
 suppressPackageStartupMessages(library(SingleCellSignalR))
 
 print("CreateDatabase on Request")
-database::createDatabase()
+#createDatabase()
 
 print("checkLastVersion FALSE")
-database::checkLastVersion(update=FALSE)
+#checkLastVersion(update=FALSE)
 
 print("checkLastVersion TRUE")
-database::checkLastVersion(update=TRUE)
+#checkLastVersion(update=TRUE)
+
+print("getInteractions")
+
+interactions <- getInteractions()
+head(interactions)
+dim(interactions)
+
+getInteractions(idRelease=1)
+head(interactions)
+dim(interactions)
 
 
-
-
-
+getInteractions(idRelease=65)
