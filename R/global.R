0ecc985f5f35e70fac1a89abf829a2d854904f89
jcolinge <65902924+jcolinge@users.noreply.github.com>
2025-09-27 20:56:47 +0200

    update R code

--- R/global.R
+++
@@ -1,7 +0,0 @@
-#' @import BiocManager
-#' @import stringr
-#' @import scran
-#' @import utils
-
-utils::globalVariables(c("LRdb", "PwC_ReactomeKEGG", "c1", "c2", 
-	"markers_default", "mm2Hs", "pw", "E<-", "V<-"))
